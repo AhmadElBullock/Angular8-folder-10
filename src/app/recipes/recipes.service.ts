b46f0f43124e178c97e0a84d889fe28648e4d0d1
AhmadElBullock <ahmadelbullock@gmail.com>
2020-07-05 20:25:41 +0200

    filling Recipe[] with Ingredients in recipe-detail.cmp and displaying them in recipe-detail.html

--- src/app/recipes/recipes.service.ts
+++ src/app/recipes/recipes.service.ts
@@ -1,10 +1,13 @@
 import { Recipe } from './recipe.model';
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipesService {
     recipes: Recipe[] = [
-        new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg', []),
-        new Recipe('A Test Recipe2', 'This is simply a test2', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg', [])
+        new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg',
+        [new Ingredient('Rice', 2), new Ingredient('Potato', 3)]),
+        new Recipe('A Test Recipe2', 'This is simply a test2', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg', 
+        [new Ingredient('Meat', 2), new Ingredient('Tomato', 3)])
       ];
 
       getRecipes() {
